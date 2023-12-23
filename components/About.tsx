18ca1fb44bb1fe530c84970f956cfb4535d186dd
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-23 22:24:19 +0500

    Completed About component

---
+++ components/About.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+	return (
+		<section
+			className="w-full bg-slate-600 h-screen flex items-center justify-center"
+			id="about">
+			<h1 className="text-[30rem] text-black bg-white px-[20rem] py-[10rem] rounded-lg">
+				ABOUT
+			</h1>
+		</section>
+	);
+}
