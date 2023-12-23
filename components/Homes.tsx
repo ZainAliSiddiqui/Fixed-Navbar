f5ecc78bac80166ab977be137160e346c0d2f0b7
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-23 22:24:14 +0500

    Completed Home component

---
+++ components/Homes.tsx
@@ -0,0 +1,11 @@
+export default function Homes() {
+	return (
+		<section
+			className="w-full bg-slate-900 h-[90vh] flex items-center justify-center"
+			id="home">
+			<h1 className="text-[30rem] text-black bg-white px-[20rem] py-[10rem] rounded-lg">
+				Home
+			</h1>
+		</section>
+	);
+}
