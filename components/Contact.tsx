51887185e15dd4580f05d034dc59eaba173e6aa5
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-23 22:24:39 +0500

    Completed Contact component

---
+++ components/Contact.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+	return (
+		<section
+			className="w-full bg-slate-900 h-screen flex items-center justify-center"
+			id="contact">
+			<h1 className="text-[30rem] text-black bg-white px-[20rem] py-[10rem] rounded-lg">
+				CONTACT
+			</h1>
+		</section>
+	);
+}
